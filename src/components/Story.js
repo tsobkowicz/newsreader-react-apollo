8cab205b16d5a0af27561bf2881004139c88b072
Tom <tomaszsobkowicz21@gmail.com>
2020-10-12 13:04:48 +0200

    Add styling, create mapTime func

--- src/components/Story.js
+++ src/components/Story.js
@@ -2,6 +2,13 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { getStory } from '../services/hnApi';
+import {
+  StoryWrapper,
+  StoryTitle,
+  StoryMeta,
+  StoryMetaElement,
+} from '../styles/StoryStyles';
+import { mapTime } from '../mappers/mapTime';
 
 export const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
@@ -11,13 +18,20 @@ export const Story = ({ storyId }) => {
   }, []);
 
   return story?.url ? (
-    <>
-      <a href={story.url}>
-        <p>{story.title}</p>
-      </a>
-      By: <p>{story.by}</p>
-      Posted: <p>{story.time}</p>
-    </>
+    <StoryWrapper data-testid="story">
+      <StoryTitle>
+        <a href={story.url}>{story.title}</a>
+      </StoryTitle>
+      <StoryMeta>
+        <span data-testid="story-by">
+          <StoryMetaElement color="#000">By:</StoryMetaElement> {story.by}
+        </span>
+        <span data-testid="story-time">
+          <StoryMetaElement color="#000">Posted:</StoryMetaElement>{' '}
+          {mapTime(story.time)}
+        </span>
+      </StoryMeta>
+    </StoryWrapper>
   ) : null;
 };
 
--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { getStoryIds } from '../services/hnApi';
 import { Story } from '../components/Story';
+import {
+  StoriesContainerWrapper,
+  GlobalStyle,
+} from '../styles/StoriesContainerStyles';
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
@@ -9,5 +13,15 @@ export const StoriesContainer = () => {
     getStoryIds().then((data) => setStoryIds(data));
   }, []);
 
-  return storyIds.map((storyId) => <Story key={storyId} storyId={storyId} />);
+  return (
+    <>
+      <GlobalStyle />
+      <StoriesContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.map((storyId) => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoriesContainerWrapper>
+    </>
+  );
 };
